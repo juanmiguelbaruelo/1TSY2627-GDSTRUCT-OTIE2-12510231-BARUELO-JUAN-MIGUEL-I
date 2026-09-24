c8d0f893ef1b91682c5c1e781dc7e78c4dc4f919
bluu-asa <juanmiguel.baruelo@benilde.edu.ph>
2026-09-24 21:03:34 +0800

    Quiz 1 REAL

--- Bubble/src/Main.java
+++ Bubble/src/Main.java
@@ -1,33 +1,58 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        int[] numbers = new int[]{400, 90, 80, 70, 50, 9, 80, 45, 55, -73};
+public class Main
+{
+    //int main()
+    public static void main(String[] args)
+    {
+        //c++ int numbers[5]
+        int[] numbers = new int[10];
+
+        numbers[0] = 400;
+        numbers[1] = 90;
+        numbers[2] = 80;
+        numbers[3] = 70;
+        numbers[4] = 50;
+        numbers[5] = 9;
+        numbers[6] = 80;
+        numbers[7] = 45;
+        numbers[8] = 55;
+        numbers[9] = -73;
+
         System.out.println("Before Bubble Sort");
-
-        for(int i = 0; i < numbers.length; ++i) {
-            System.out.print(numbers[i] + " ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.print(numbers[i]+ " ");
         }
 
-        for(int lastSortedIndex = numbers.length - 1; lastSortedIndex < 0; --lastSortedIndex) {
-            for(int i = 0; i < lastSortedIndex; ++i) {
+        //BubbleSort O(n^2)
+        for (int lastSortedIndex = numbers.length - 1;
+             lastSortedIndex > 0;
+             lastSortedIndex--)
+        {
+            //i = current index
+            for(int i = 0; i < lastSortedIndex; i++)
+            {
                 int leftNum = numbers[i];
-                int rightNum = numbers[i + 1];
-                if (leftNum > rightNum) {
+                int rightNum = numbers[i+1];
+
+                if(leftNum < rightNum)
+                {
                     numbers[i] = rightNum;
-                    numbers[i + 1] = leftNum;
+                    numbers[i+1] = leftNum;
+                    //System.out.print(numbers[i] + " : " + numbers[i+1]);
                 }
             }
         }
 
         System.out.println("\nAfter Bubble Sort");
-
-        for(int i = 0; i < numbers.length; ++i) {
-            System.out.print(numbers[i] + " ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.print(numbers[i]+ " ");
         }
 
+
     }
+
+
 }
--- Selection/src/Main.java
+++ Selection/src/Main.java
@@ -1,36 +1,62 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        int[] numbers = new int[]{400, 90, 80, 70, 50, 9, 80, 45, 55, -73};
+public class Main
+{
+    //int main()
+    public static void main(String[] args)
+    {
+        //c++ int numbers[5]
+        int[] numbers = new int[10];
+
+        numbers[0] = 400;
+        numbers[1] = 90;
+        numbers[2] = 80;
+        numbers[3] = 70;
+        numbers[4] = 50;
+        numbers[5] = 9;
+        numbers[6] = 80;
+        numbers[7] = 45;
+        numbers[8] = 55;
+        numbers[9] = -73;
+
         System.out.println("Before Selection Sort");
-
-        for(int i = 0; i < numbers.length; ++i) {
-            System.out.print(numbers[i] + " ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.print(numbers[i]+ " ");
         }
 
-        for(int curIdx = 0; curIdx < numbers.length; ++curIdx) {
+        //SelectionSort O(n^2)
+        for(int curIdx = 0; curIdx < numbers.length; curIdx++)
+        {
             int largestIndex = curIdx;
 
-            for(int searchIdx = curIdx + 1; searchIdx < numbers.length; ++searchIdx) {
-                if (numbers[searchIdx] > numbers[largestIndex]) {
+            //search per index
+            for (int searchIdx = curIdx + 1;
+                 searchIdx<numbers.length;
+                 searchIdx++)
+            {
+                //replacing the value in the largest index
+                if(numbers[searchIdx] > numbers[largestIndex])
+                {
                     largestIndex = searchIdx;
                 }
             }
 
+            //swapping
             int largestNumber = numbers[largestIndex];
             numbers[largestIndex] = numbers[curIdx];
             numbers[curIdx] = largestNumber;
         }
 
-        System.out.println("\nAfter Selection Sort");
 
-        for(int i = 0; i < numbers.length; ++i) {
-            System.out.print(numbers[i] + " ");
+        System.out.println("\nAfter Selection Sort");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.print(numbers[i]+ " ");
         }
 
+
     }
+
+
 }
